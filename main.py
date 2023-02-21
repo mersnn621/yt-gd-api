9c7a6fb27361593acc70b003a5cbc2a0d4acd65d
mersnn621 <mersnn621@gmail.com>
2023-02-21 20:39:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from yt_dlp import YoutubeDL
 import sys
 import subprocess
@@ -8,8 +9,12 @@ app = FastAPI()
 
 @app.get("/")
 def read_item(v: str = None):
-    videoid = v
-    print(videoid)
-    cmd = 'yt-dlp -g "https://www.youtube.com/watch?v="'+videoid
-    process = (subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True).communicate()[0]).decode('utf-8')
-    return {"video": process.splitlines()[0],"audio":process.splitlines()[1]}
+    try:
+        videoid = v
+        cmd = 'yt-dlp -g "https://www.youtube.com/watch?v="'+videoid
+        process = (subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True).communicate()[0]).decode('utf-8')
+        content = {"status":"Success","video": process.splitlines()[0],"audio":process.splitlines()[1]}
+        headers = {"Access-Control-Allow-Origin": "*"}
+        return JSONResponse(content=content, headers=headers)
+    except:
+        return {"status":"Failed"}
